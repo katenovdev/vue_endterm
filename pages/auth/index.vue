83e7ecacb72e746573dfd535b948e56299c8884c
katenovdev <katenovdev@gmail.com>
2024-12-14 14:05:12 +0500

    first commit

---
+++ pages/auth/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <h1>Welcome to Auth Page</h1>
+      <nuxt-link to="/about">Go to About Page</nuxt-link>
+    </div>
+  </template>
+  
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to My Nuxt App</h1>
+    <nuxt-link to="/about">Go to About Page</nuxt-link>
+  </div>
+</template>
